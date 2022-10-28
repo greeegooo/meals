acec976b3dbdecb78795247510f87987ec096fca
greeegooo <gregomicha@gmail.com>
2022-10-28 09:42:41 -0300

    Fix many to one

--- src/meals/entities/meal.entity.ts
+++ src/meals/entities/meal.entity.ts
@@ -1,12 +1,12 @@
 import { User } from "../../auth/entities/user.entity";
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({ name: 'meals' })
 export class Meal {
     @PrimaryGeneratedColumn("uuid")
     id: string;
 
-    @OneToOne(() => User, { eager: true })
+    @ManyToOne(() => User, { eager: true })
     @JoinColumn()
     chef: User;
 
